7684a827d7c97819a860ae8bd337822a09d02872
vasyl savitskyy home <vasylsavitskyy@gmail.com>
2020-07-16 00:59:14 +0300

    add bootstrap and react-router-dom, update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import {Link} from 'react-router-dom';
 
-function App() {
+import './App.scss';
+
+type ExitType = () => void;
+
+interface Props {
+  title: string
+  exit: ExitType
+  titleBtnBack: string
+  titleBtnExit: string
+  emptyFilialList: string
+  titleAppointTask: string
+  titleInWork: string
+}
+
+function App(props: Props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Fragment>
+      <header className="container-fluid shadow-sm mb-3 border header">
+        <div className="row">
+          <div className="col col-md-3 middle">
+            <h1 className="title">{props.title}</h1>
+          </div>
+
+          <div className="col-6 col-md-3 order-md-1 justify-content-end middle">
+            <Link
+              className="btn btn-link d-md-none"
+              to="#"
+              title={props.titleBtnBack}
+            >
+              <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"/>
+                <path fillRule="evenodd" d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+            </Link>
+            <button
+              className="btn btn-sm btn-primary"
+              onClick={props.exit}
+              title={props.titleBtnExit}
+            >Выход</button>
+          </div>
+
+          <div className="col-12 col-md-6 middle">
+            <fieldset className="border pl-2 pr-2">
+              <legend className="m-0 w-auto">Филиалы</legend>
+              <span className="font-weight-bold">{props.emptyFilialList}</span>
+            </fieldset>
+          </div>
+        </div>
       </header>
-    </div>
+
+      <main className="content">
+        <section className="shadow-sm mb-3 border appoint">
+          <h2>
+            {props.titleAppointTask}
+            <span className="ml-2 text-success">
+              <svg width=".5em" height="1em" viewBox="0 0 16 16" className="bi bi-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="8" cy="8" r="8"/>
+              </svg>
+            </span>
+          </h2>
+
+        </section>
+
+        <section className="shadow-sm border in-work">
+          <h2>
+            {props.titleInWork}
+            <span className="ml-2 text-warning">
+              <svg width=".5em" height="1em" viewBox="0 0 16 16" className="bi bi-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="8" cy="8" r="8"/>
+              </svg>
+            </span>
+          </h2>
+        </section>
+      </main>
+    </Fragment>
   );
+}
+
+App.defaultProps = {
+  title: "Диспетчерская",
+  exit: (): void => { console.warn('realise action: exit')},
+  titleBtnBack: "вернутся к модулям",
+  titleBtnExit: "к авторизации",
+  emptyFilialList: "Данных нет!",
+  titleAppointTask: "Назначить задачу:",
+  titleInWork: "Задачи в работе:"
 }
 
 export default App;
